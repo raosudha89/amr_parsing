53ed336473e88b2a179eab8918a8bef77bb3f209
Sudha Rao <raosudha@umiacs.umd.edu>
2014-11-01 16:01:47 -0400

    Changed oracle to just a single oracle action

--- concept_relation.py
+++ concept_relation.py
@@ -1,10 +1,12 @@
-import os, sys
+import sys, os
+import cPickle as pickle
 
-VW_PYTHON_PATH = os.environ['VW_PYTHON_PATH']
-if not VW_PYTHON_PATH:
+if not os.environ.has_key('VW_PYTHON_PATH'):
 	print "Please set the env var VW_PYTHON_PATH to point to location of vowpal_wabbit/python"
 	sys.exit()	
+VW_PYTHON_PATH = os.environ['VW_PYTHON_PATH']
 sys.path.append(VW_PYTHON_PATH)
+
 import pyvw
 
 class Span:
@@ -24,15 +26,22 @@ training_sentence = [
 
 def getKbestConcepts(span):
 	#Get the top k concepts aligned with span.words
-	return ["NULL", "establish-01", "model", "industry", "innovate-01"]
+	K = 5
+	span_concept_dict = pickle.load(open("span_concept_dict.p", "rb"))
+	if span_concept_dict.has_key(span.words):
+		return [ concept for (concept, count) in span_concept_dict[span.words]][:K] 
+	return ["NULL"]
 
-concept2label = {
-	"NULL": 1,
-	"establish-01" : 2,
-	"model" : 3,
-	"industry" : 4,
-	"innovate-01" : 5,
-}	
+concept_labels = {"NULL": 1}
+last_used_label = 1
+
+def concept2label(concept):
+	global concept_labels
+	global last_used_label
+	if not concept_labels.has_key(concept):
+		concept_labels[concept] = last_used_label + 1
+		last_used_label += 1
+	return concept_labels[concept]  
 
 def get_words(sentence, i): 
 	return "<s>" if i < 0 else "</s>" if i >= len(sentence) else sentence[i].words
@@ -65,33 +74,48 @@ class Concept_Relation(pyvw.SearchTask):
             }
 		#print f
 		ex = self.vw.example(f, labelType=self.vw.lCostSensitive)
-		label = concept2label[concept]
+		label = concept2label(concept)
 		ex.set_label_string(str(label) + ":0")
 		return ex
 
 	def _run(self, sentence):
+		#print [span.words for span in sentence]
 		output = []
 		for i in range(len(sentence)):
 			span = sentence[i]
 			k_best_concepts = getKbestConcepts(span)
 			#print "Concepts: ", k_best_concepts 
 			examples = [self.makeConceptExample(sentence, i, concept) for concept in k_best_concepts]
-			oracle = concept2label[span.concept]
+			oracles = [concept2label(span.concept) for concept in k_best_concepts]
 			pred = self.sch.predict(examples = examples,
-			                        my_tag = 0,
-			                        oracle = oracle)
-			#print concept2label[span.concept], pred
+									my_tag = 0,
+									oracle = oracles)
+			#print concept2label(span.concept), pred
 			output.append(pred)
 		#print output
 		return output
 
 def main(argv):
+	#Format of concept_training_dataset
+	#concept_training_dataset = {id: [span, pos, concept]}
+	concept_training_dataset = pickle.load(open("concept_training_dataset.p", "rb")) 
+	training_sentences = []
+	for id, concept_training_data in concept_training_dataset.iteritems():
+		training_sentence = []
+		for [span, pos, concept] in concept_training_data:
+			training_sentence.append(Span(span, pos, concept))
+		training_sentences.append(training_sentence)
+	N = int(len(training_sentences) * 0.9)
+	N = 10
 	vw = pyvw.vw("--search 0 --csoaa_ldf m --quiet --search_task hook --ring_size 2048 --search_no_caching -q sc")
 	task = vw.init_search_task(Concept_Relation)
-	for p in range(2): task.learn([training_sentence].__iter__)
+	print "Learning.."
+	for p in range(2):
+		task.learn(training_sentences[:N].__iter__)
+	test_sentences = training_sentences[N:N+2]
 	print 'test time'
-	print task.predict(eraseAnnotations(training_sentence))
-	print "should have printed [2, 3, 1, 4, 5]"	
+	for test_sentence in test_sentences:
+		print task.predict(eraseAnnotations(test_sentence))
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
