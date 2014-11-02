965acf17f71972147fa100a5f56962be14ad0ec9
Sudha Rao <raosudha@umiacs.umd.edu>
2014-11-02 18:57:34 -0500

    Concept Relation learning code for entire training set

--- concept_relation.py
+++ concept_relation.py
@@ -1,5 +1,6 @@
 import sys, os
 import cPickle as pickle
+import time
 
 if not os.environ.has_key('VW_PYTHON_PATH'):
 	print "Please set the env var VW_PYTHON_PATH to point to location of vowpal_wabbit/python"
@@ -16,14 +17,6 @@ class Span:
 		self.concept = concept
 		self.parents = parents
 		
-training_sentence = [
-	Span("Establishing", "VBG", "establish-01"),
-	Span("Models", "NNS", "model", [0]),
-	Span("in", "IN"),
-	Span("Industrial", "NNP", "industry", [1]),
-	Span("Innovation", "NNP", "innovate-01", [3])
-]
-
 def getKbestConcepts(span):
 	#Get the top k concepts aligned with span.words
 	K = 5
@@ -55,6 +48,9 @@ def eraseAnnotations(sentence):
         erasedSentence.append( Span(sentence[i].words, sentence[i].pos) )
     return erasedSentence
 
+def get_concept_features(concept):
+	return concept.split('-')
+
 class Concept_Relation(pyvw.SearchTask):
 	def __init__(self, vw, sch, num_actions):
 		pyvw.SearchTask.__init__(self, vw, sch, num_actions)
@@ -69,9 +65,10 @@ class Concept_Relation(pyvw.SearchTask):
                    [ "w@-" + str(delta) + "=" + get_words(sentence,i-delta) for delta in [1,2] ] +
                    [ "w@+" + str(delta) + "=" + get_words(sentence,i+length+delta-1) for delta in [1,2] ] +
                    [ "p@-" + str(delta) + "=" + get_pos(sentence,i-delta) for delta in [1,2] ] +
-                   [ "p@+" + str(delta) + "=" + get_pos(sentence,i+length+delta-1) for delta in [1,2] ],
-              'c': [ 'concept=' + concept ]
-            }
+                   [ "p@+" + str(delta) + "=" + get_pos(sentence,i+length+delta-1) for delta in [1,2] ] +
+				   [ "boc=" + c for c in getKbestConcepts(sentence[i])],
+              'c': get_concept_features(concept)
+			}
 		#print f
 		ex = self.vw.example(f, labelType=self.vw.lCostSensitive)
 		label = concept2label(concept)
@@ -83,17 +80,19 @@ class Concept_Relation(pyvw.SearchTask):
 		output = []
 		for i in range(len(sentence)):
 			span = sentence[i]
-			k_best_concepts = getKbestConcepts(span)
-			#print "Concepts: ", k_best_concepts 
+			examples = []
+			oracles = []
+			k_best_concepts = getKbestConcepts(sentence[i])
 			examples = [self.makeConceptExample(sentence, i, concept) for concept in k_best_concepts]
-			oracles = [concept2label(span.concept) for concept in k_best_concepts]
+			oracle = [ v for v,concept in enumerate(k_best_concepts)  if concept == span.concept ]
 			pred = self.sch.predict(examples = examples,
 									my_tag = 0,
-									oracle = oracles)
-			#print concept2label(span.concept), pred
-			output.append(pred)
-		#print output
+									oracle = oracle)
+			output.append( concept2label(k_best_concepts[pred]) )
 		return output
+
+def get_true_concepts(sentence):
+	return [concept2label(span.concept) for span in sentence]
 
 def main(argv):
 	#Format of concept_training_dataset
@@ -106,16 +105,24 @@ def main(argv):
 			training_sentence.append(Span(span, pos, concept))
 		training_sentences.append(training_sentence)
 	N = int(len(training_sentences) * 0.9)
-	N = 10
+	#N = 10
 	vw = pyvw.vw("--search 0 --csoaa_ldf m --quiet --search_task hook --ring_size 2048 --search_no_caching -q sc")
 	task = vw.init_search_task(Concept_Relation)
 	print "Learning.."
+	start_time = time.time()
 	for p in range(2):
-		task.learn(training_sentences[:N].__iter__)
-	test_sentences = training_sentences[N:N+2]
-	print 'test time'
+		task.learn(training_sentences[:N])
+	print "Time taken: " + str(time.time() - start_time)
+	#test_sentences = training_sentences[N:15]
+	test_sentences = training_sentences[N:]
+	start_time = time.time()
+	print "Testing.."
+	print len(test_sentences) 
 	for test_sentence in test_sentences:
 		print task.predict(eraseAnnotations(test_sentence))
+		print "should have printed"
+		print get_true_concepts(test_sentence)
+	print "Time taken: " + str(time.time() - start_time)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
